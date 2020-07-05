9c4642f31cb8096574a71ab631f37a4f1bf89985
GIHOON <rlgns98kr@gmail.com>
2020-07-06 01:30:49 +0900

    Change app name

--- app/src/main/java/com/gihoon/richardallright/Map.java
+++ app/src/main/java/com/gihoon/richardallright/Map.java
@@ -136,7 +136,6 @@ public class Map extends AppCompatActivity implements OnMapReadyCallback, Google
         if (currentUser.getPhotoUrl() != null) {
             Glide.with(getApplication()).load(currentUser.getPhotoUrl()).into(iv);
         }
-        System.out.println("aa" + currentUser.getDisplayName());
         if (currentUser.getDisplayName() != null) {
             if (!currentUser.getDisplayName().equals("")) {
                 tv.setText(currentUser.getDisplayName());
